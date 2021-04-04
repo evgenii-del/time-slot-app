435f8c0adbfe970822670e99d0ac7a9974bfbca2
Khasanov <zkhasanovv@gmail.com>
2021-04-04 16:13:56 +0300

    SlotControlller. Added methods for creating and getting selected time slots

--- server/controllers/slotController.js
+++ server/controllers/slotController.js
@@ -1,8 +1,16 @@
+const {Slot} = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class SlotController {
     async saveSelectedSlot(req, res) {
+        const {value} = req.body;
+        const slot = await Slot.create({value});
+        return res.json(slot);
     };
 
     async returnSelectedSlot(req, res) {
+        const slots = await Slot.findAll();
+        return res.json(slots);
     };
 }
 
