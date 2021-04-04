e782cf773b2879d7c1ad1086256e5ed7879f807a
Khasanov <zkhasanovv@gmail.com>
2021-04-04 17:12:21 +0300

    UserController. Added methods for registration and authorization user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,44 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const ApiError = require('../error/ApiError');
+const {User} = require('../models/models');
+
+const generateJwt = (id, username) => {
+    return jwt.sign(
+        {id, username},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    );
+}
+
 class UserController {
-    async authorization(req, res) {
+    async registration(req, res, next) {
+        const {username, password} = req.body;
+        if (!username || !password) {
+            return next(ApiError.badRequest('Wrong username or password'));
+        }
+        const candidate = await User.findOne({where: {username}});
+        if (candidate) {
+            return next(ApiError.badRequest('User with this username already exists'));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({username, password: hashPassword});
+        const token = generateJwt(user.id, username);
+        return res.json({token});
+    };
+
+    async authorization(req, res, next) {
+        const {username, password} = req.body;
+        const candidate = await User.findOne({where: {username}});
+        if (!candidate) {
+            return next(ApiError.badRequest('Username and password are wrong'));
+        }
+        const comparePassword = bcrypt.compareSync(password, candidate.password);
+        if (!comparePassword) {
+            return next(ApiError.badRequest('Wrong password'));
+        }
+        const token = generateJwt(candidate.id, username);
+        return res.json({token});
     };
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 
 const router = new Router();
 
+router.post('/registration', userController.registration);
 router.post('/auth', userController.authorization);
 
 module.exports = router;
